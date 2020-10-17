6747cf34389e729fca5e3434e298a212d4579ee2
Robert Castillo <robert.castillo@gmx.at>
2020-10-17 14:36:24 +0200

    corrections

--- script.js
+++ script.js
@@ -11,36 +11,36 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
-var sightArr = [];
-var travel = /** @class */ (function () {
-    function travel(place, zipcode, address, img) {
+var attractionsArr = [];
+var Travel = /** @class */ (function () {
+    function Travel(place, zipcode, address, img) {
         this.place = place;
         this.address = address;
         this.zipcode = zipcode;
         this.img = img;
-        sightArr.push(this);
+        attractionsArr.push(this);
     }
-    travel.prototype.render = function () {
-        return "\n        <div class=\"card-deck-row text-center col-sm-11 col-md-5 bg-dark col-lg.-2 m-4 p-1 justify content-between w-100 rounded\" id=\"attracions\">\n                <img src=\"" + this.img + "\" class=\"card-img-top card-img-fluid my-2 d-none\" alt=\"...\">\n                    <div class=\"card-body\">\n                        <h5 class=\"card-title mb-2\">" + this.place + "</h5>\n                        <p class=\"address\">" + this.address + "</p>\n                        <p class=\"zipcode\"><small class=\"text-muted\">" + this.zipcode + "</small></p>\n                    </div>\n        </div>";
+    Travel.prototype.render = function () {
+        return "\n        <div class=\"card-deck-row text-center col-sm-11 col-md-5 bg-dark col-lg.-2 m-4 p-1 justify content-between w-100 rounded\" id=\"attracions\">\n                <img src=\"" + this.img + "\" class=\"card-img-top card-img-fluid my-2 d-none\" alt=\"...\">\n                    <div class=\"card-body\">\n                        <h5 class=\"card-title mb-2\">" + this.place + "</h5>\n                        <p class=\"address\">" + this.address + "</p>\n                        <p class=\"zipcode\">" + this.zipcode + "</p>\n                    </div>\n        </div>";
     };
-    return travel;
+    return Travel;
 }());
-var restaurant = /** @class */ (function (_super) {
-    __extends(restaurant, _super);
-    function restaurant(place, zipcode, address, img, food, website) {
+var Restaurant = /** @class */ (function (_super) {
+    __extends(Restaurant, _super);
+    function Restaurant(place, zipcode, address, img, food, website) {
         var _this = _super.call(this, place, zipcode, address, img) || this;
         _this.food = food;
         _this.website = website;
         return _this;
     }
-    restaurant.prototype.render = function () {
+    Restaurant.prototype.render = function () {
         return "\n        <div class=\"card-deck-row text-center col-sm-11 col-md-5 bg-dark col-lg.-2 m-4 p-1 justify content-between w-100 rounded\" id=\"attracions\">\n                <img src=\"" + this.img + "\" class=\"card-img-top card-img-fluid my-2 d-none\" alt=\"...\">\n                    <div class=\"card-body\">\n                        <h5 class=\"card-title mb-2\">" + this.place + "</h5>\n                        <p class=\"address\">" + this.address + "</p>\n                        <p class=\"zipcode\">" + this.zipcode + "</p>\n                        <p class=\"food\">" + this.food + "</p>\n                        <a href=\"" + this.website + "\" class=\"website\">Click for the website</p>\n\n                    </div>\n        </div>";
     };
-    return restaurant;
-}(travel));
-var Event = /** @class */ (function (_super) {
-    __extends(Event, _super);
-    function Event(place, zipcode, address, img, eventName, eventDate, eventTime, price, website) {
+    return Restaurant;
+}(Travel));
+var Show = /** @class */ (function (_super) {
+    __extends(Show, _super);
+    function Show(place, zipcode, address, img, eventName, eventDate, eventTime, price, website) {
         var _this = _super.call(this, place, zipcode, address, img) || this;
         _this.eventName = eventName;
         _this.eventDate = eventDate;
@@ -49,19 +49,19 @@ var Event = /** @class */ (function (_super) {
         _this.website = website;
         return _this;
     }
-    Event.prototype.render = function () {
-        return "\n                    <div class=\"card-deck-row text-center col-sm-11 col-md-5 bg-dark col-lg.-2 m-4 p-1 justify content-between w-100 rounded\" id=\"attracions\">\n                            <img src=\"" + this.img + "\" class=\"card-img-top card-img-fluid my-2 d-none\" alt=\"...\">\n                                <div class=\"card-body\">\n                                    <h5 class=\"card-title mb-2\">" + this.place + "</h5>\n                                    <p class=\"address\">" + this.address + "</p>\n                                    <p class=\"zipcode\">" + this.zipcode + "</p>\n                                    <p class=\"eventName\">" + this.eventName + "</p>\n                                    <p class=\"eventDate\">" + this.eventDate + "</p>\n                                    <p class=\"eventTime\">" + this.eventTime + "</p>\n                                    <a href=\"" + this.website + "\" class=\"website\">Click for the website</p>\n            \n                                </div>\n                    </div>";
+    Show.prototype.render = function () {
+        return "\n                    <div class=\"card-deck-row text-center col-sm-11 col-md-5 bg-dark col-lg.-2 m-4 p-1 justify content-between w-100 rounded\" id=\"attracions\">\n                            <img src=\"" + this.img + "\" class=\"card-img-top card-img-fluid my-2 d-none\" alt=\"...\">\n                                <div class=\"card-body\">\n                                    <h5 class=\"card-title mb-2\">" + this.place + "</h5>\n                                    <p class=\"address\">" + this.address + "</p>\n                                    <p class=\"zipcode\">" + this.zipcode + "</p>\n                                    <p class=\"eventName\">" + this.eventName + "</p>\n                                    <p class=\"eventDate\">" + this.eventDate + "</p>\n                                    <p class=\"eventTime\">" + this.eventTime + "</p>\n                                    <p class=\"eventPrice\">" + this.price + "</p>\n                                    <a href=\"" + this.website + "\" class=\"website\">Click for the website</p>\n            \n                                </div>\n                    </div>";
     };
-    return Event;
+    return Show;
 }(travel));
-var church = new place("St. Charles Church", "Karlsplatz 1", "1010 Vienna", "img/karlskirch.jpg");
-var zoo = new place("Zoo Vienna", "Maxingstraße 13b", "1130 Vienna", "img/zoo.jpg");
-var lemon = new restaurant("Lemon Leaf Thai Restaurant", "Kettenbrückengass 19", "1050 Vienna", "img/lemonleaf.png", "Thai kitchen", "www.lemonleaf.at");
-var sixta = new restaurant("Sixta Restaurant", "Schönbrunner Straße 21", "1050 Vienna", "img/sixta.png", "Traditional food", "www.sixta-restaurant.at");
-var kris = new Event("Kris Kristofferson", "Wiener Stadthalle,HAlle F, Roland Rainer Platz 1", "1150 Vienna", "img/Kristofferson.jpg", "Thursday 15.11.2021", "20:00", "58.50EUR", "www.kriskristofferson.com");
-var lenny = new Event("Lenny Kravitz", "Wiener Stadthalle, Halle D, roland Rainer Platz 1", "1150 Vienna", "img/lenny.jpg", "Saturday 09.12.2029", "19:30", "47.80EUR", "www.lennykravitz.com");
+var church = new Travel("St. Charles Church", "Karlsplatz 1", "1010 Vienna", "img/karlskirch.jpg");
+var zoo = new Travel("Zoo Vienna", "Maxingstraße 13b", "1130 Vienna", "img/zoo.jpg");
+var apple = new Restaurant("Lemon Leaf Thai Restaurant", "Kettenbrückengass 19", "1050 Vienna", "img/lemonleaf.png", "Thai kitchen", "www.lemonleaf.at");
+var center = new Restaurant("Sixta Restaurant", "Schönbrunner Straße 21", "1050 Vienna", "img/sixta.png", "Traditional food", "www.sixta-restaurant.at");
+var artist = new Show("img/Kristofferson.jpg", "Wiener Stadthalle,HAlle F", "Roland Rainer Platz 1", "1150 Vienna", "Kris Kristofferson", "Thursday 15.11.2021", "20:00", "58.50EUR", "www.kriskristofferson.com");
+var star = new Show("img/lenny.jpg", "Wiener Stadthalle, Halle D", "Roland Rainer Platz 1", "1150 Vienna", "Lenny Kravitz", "Saturday 09.12.2029", "19:30", "47.80EUR", "www.lennykravitz.com");
 $(document).ready(function () {
-    for (var i in sightArr) {
-        document.getElementById("attractions").innerHTML += sightArr[i].render();
+    for (var i in attractionsArr) {
+        document.getElementById("attractions").innerHTML += attractionsArr[i].render();
     }
 });
--- script.ts
+++ script.ts
@@ -1,6 +1,6 @@
- const sightArr: Array<any>=[];
+ const attractionsArr: Array<any>=[];
 
-    class travel{
+    class Travel{
         place: string;
         address: string;
         zipcode: string;
@@ -12,7 +12,7 @@
         this.address = address;
         this.zipcode = zipcode;
         this.img = img;
-        sightArr.push(this);
+        attractionsArr.push(this);
 
     }
     render() {
@@ -22,13 +22,13 @@
                     <div class="card-body">
                         <h5 class="card-title mb-2">${this.place}</h5>
                         <p class="address">${this.address}</p>
-                        <p class="zipcode"><small class="text-muted">${this.zipcode}</small></p>
+                        <p class="zipcode">${this.zipcode}</p>
                     </div>
         </div>`
     }
 }
 
-    class restaurant extends travel{
+    class Restaurant extends Travel{
         food: string;
         website: string;
 
@@ -55,7 +55,7 @@
         </div>`
      }
 }
-    class Event extends travel{
+    class Show extends Travel{
             eventName: string;
             eventDate: string;
             eventTime: string;
@@ -84,6 +84,7 @@
                                     <p class="eventName">${this.eventName}</p>
                                     <p class="eventDate">${this.eventDate}</p>
                                     <p class="eventTime">${this.eventTime}</p>
+                                    <p class="eventPrice">${this.price}</p>
                                     <a href="${this.website}" class="website">Click for the website</p>
             
                                 </div>
@@ -92,16 +93,16 @@
     } 
 
 
-var church = new place("St. Charles Church","Karlsplatz 1", "1010 Vienna", "img/karlskirch.jpg");
-var zoo = new place("Zoo Vienna", "Maxingstraße 13b", "1130 Vienna", "img/zoo.jpg");
-var lemon = new restaurant("Lemon Leaf Thai Restaurant", "Kettenbrückengass 19", "1050 Vienna", "img/lemonleaf.png", "Thai kitchen", "www.lemonleaf.at");
-var sixta = new restaurant("Sixta Restaurant", "Schönbrunner Straße 21", "1050 Vienna", "img/sixta.png", "Traditional food", "www.sixta-restaurant.at");
-var kris = new Event("Kris Kristofferson","Wiener Stadthalle,HAlle F, Roland Rainer Platz 1", "1150 Vienna", "img/Kristofferson.jpg", "Thursday 15.11.2021", "20:00","58.50EUR", "www.kriskristofferson.com");
-var lenny = new Event("Lenny Kravitz","Wiener Stadthalle, Halle D, roland Rainer Platz 1", "1150 Vienna", "img/lenny.jpg", "Saturday 09.12.2029", "19:30", "47.80EUR", "www.lennykravitz.com");
+var church = new Travel("St. Charles Church","Karlsplatz 1", "1010 Vienna", "img/karlskirch.jpg");
+var zoo = new Travel("Zoo Vienna", "Maxingstraße 13b", "1130 Vienna", "img/zoo.jpg");
+var apple = new Restaurant("Lemon Leaf Thai Restaurant", "Kettenbrückengass 19", "1050 Vienna", "img/lemonleaf.png", "Thai kitchen", "www.lemonleaf.at");
+var center = new Restaurant("Sixta Restaurant", "Schönbrunner Straße 21", "1050 Vienna", "img/sixta.png", "Traditional food", "www.sixta-restaurant.at");
+var artist = new Show("img/Kristofferson.jpg","Wiener Stadthalle,HAlle F", "Roland Rainer Platz 1", "1150 Vienna","Kris Kristofferson", "Thursday 15.11.2021", "20:00","58.50EUR", "www.kriskristofferson.com");
+var star = new Show("img/lenny.jpg", "Wiener Stadthalle, Halle D", "Roland Rainer Platz 1","1150 Vienna", "Lenny Kravitz", "Saturday 09.12.2029", "19:30", "47.80EUR", "www.lennykravitz.com");
 
 $(document).ready(function(){
-    for(let i in sightArr){
-        document.getElementById("attractions").innerHTML += sightArr[i].render();
+    for(let i in attractionsArr){
+        document.getElementById("attractions").innerHTML += attractionsArr[i].render();
     
 }
  })
